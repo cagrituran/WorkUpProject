23647f604aefa43ff7370b4793e4bbbb1e158ecd
ESRA ERGÜN <esraergunnn@outlook.com>
2022-05-30 14:21:46 +0300

    sa

--- src/components/UserRegister.js
+++ src/components/UserRegister.js
@@ -4,12 +4,12 @@
 
 
 
-    
+
 
 
 // function UserRegister() {
 //     const navigate = useNavigate();
-    
+
 //     const url = "https://localhost:7079/api/User/register"
 //     const[data ,setData] = useState({
 //         name:"",
@@ -30,7 +30,7 @@
 //         }).then(res=>{
 //             console.log(res.data)    
 //             navigate('/loginuser'); 
-                                             
+
 //         }).catch(function(error){
 //             if(error.response){
 //                 console.log(error.response.data);
@@ -38,18 +38,18 @@
 //                 console.log(error.response.headers);
 //             }
 //         })
-        
-        
- 
-     
+
+
+
+
 //     }
 //     function handle(e){
 //         const newdata = {...data}
 //         newdata[e.target.id]=e.target.value
 //         setData(newdata)
 //         console.log(newdata)
- 
- 
+
+
 //     }
 //   return (
 //     <div>
@@ -66,7 +66,7 @@
 //         <br></br>
 //         <button>submit</button>
 //     </form>
-    
+
 // </div>
 //   )
 // }
@@ -77,147 +77,158 @@ import Axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
 
 
-    
+
 
 
 function UserRegister() {
     const navigate = useNavigate();
-    
-    const url = "https://localhost:7079/api/User/register"
-    const[data ,setData] = useState({
-        name:"",
-        lastname:"",      
-        email:"",
-        password:"",
-        newpassword:""
-    })
-    function submit(e){
-         e.preventDefault();
-        Axios.post(url,{
-            name:data.name,
-            lastname:data.lastname,
-            email:data.email,
-            password:data.password,
-            newpassword:data.newpassword
 
-        }).then(res=>{
-            console.log(res.data)    
-            navigate('/loginuser'); 
-                                             
-        }).catch(function(error){
-            if(error.response){
+    const url = "https://localhost:7079/api/User/register"
+    const [data, setData] = useState({
+        name: "",
+        lastname: "",
+        email: "",
+        password: "",
+        newpassword: ""
+    })
+    function submit(e) {
+        e.preventDefault();
+        Axios.post(url, {
+            name: data.name,
+            lastname: data.lastname,
+            email: data.email,
+            password: data.password,
+            newpassword: data.newpassword
+
+        }).then(res => {
+            console.log(res.data)
+            navigate('/loginuser');
+
+        }).catch(function (error) {
+            if (error.response) {
                 console.log(error.response.data);
                 console.log(error.response.status);
                 console.log(error.response.headers);
             }
         })
-        
-        
- 
-     
+
+
+
+
     }
-    function handle(e){
-        const newdata = {...data}
-        newdata[e.target.id]=e.target.value
+    function handle(e) {
+        const newdata = { ...data }
+        newdata[e.target.id] = e.target.value
         setData(newdata)
         console.log(newdata)
- 
- 
+
+
     }
-  return (
-    <section className="h-100">
-    <div className="container h-100">
-        <div className="row justify-content-sm-center h-100">
-            <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-                <div className="text-center my-5">
-                    <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100"></img>
-                </div>
-                <div className="card shadow-lg">
-                    <div className="card-body p-5 ">
-                        <h1 className="fs-4 card-title fw-bold mb-4">Register</h1>
-                        {/* <form method="POST" class="needs-validation" novalidate="" autocomplete="off"> */}
-                        <form  onSubmit={(e)=>submit(e)} className="my-login-validation" noValidate="">
-                            <div className="mb-3">
-                                <label className="mb-1 text-body " htmlFor="name">Name</label>
-                                <br></br>
-                                <input onChange={(e)=> handle(e)} id="name" value={data.name} placeholder="Enter your name here..." type="text"></input>
-                                <div className="invalid-feedback">
-                                    Name is required	
+    return (
+        <Container>
+            <div className="my-login-page">
+                <Nav>
+                    <a href="/">
+                        <img src="/images/login-logo.svg" alt="" />
+                    </a>
+
+                </Nav>
+                <section className="h-100">
+                    <div className="container h-100">
+                        <div className="row justify-content-md-center h-100">
+                            <div className="card-wrapper">
+                                {/* <div className="brand">
+                            <img src="/images/login-logo.svg" alt="logo"/>
+                        </div> */}
+                                <div className="card fat">
+                                    <div className="card-body">
+                                        <h4 className="card-title">Kayıt Ol</h4>
+                                        <p className="card-title">Workup Dünyasına Hoşgeldin</p>
+                                        <form onSubmit={(e) => submit(e)} className="my-login-validation" noValidate="">
+                                            <div className="form-group">
+                                                {/* <label htmlFor="email">E-posta Adresi</label> */}
+                                                <input onChange={(e) => handle(e)} id="name" placeholder="İsim" type="name" className="form-control" name="name" value={data.name} required autoFocus />
+                                                <div className="invalid-feedback">
+                                                    İsim Girmek Zorunludur!
+                                                </div>
+                                            </div>
+                                            <div className="form-group">
+                                                {/* <label htmlFor="email">E-posta Adresi</label> */}
+                                                <input onChange={(e) => handle(e)} id="lastname" placeholder="Soyisim" type="lastname" className="form-control" name="lastname" value={data.lastname} required autoFocus />
+                                                <div className="invalid-feedback">
+                                                    Soyisim Girmek Zorunludur!
+                                                </div>
+                                            </div>
+
+                                            <div className="form-group">
+                                                {/* <label htmlFor="email">E-posta Adresi</label> */}
+                                                <input onChange={(e) => handle(e)} id="email" placeholder="E posta Adresi" type="email" className="form-control" name="email" value={data.email} required autoFocus />
+                                                <div className="invalid-feedback">
+                                                    E-posta Zorunludur!!
+                                                </div>
+                                            </div>
+                                            <div className="form-group">
+                                                {/* <label htmlFor="password">Şifre */}
+                                                
+                                                {/* </label> */}
+                                                <input onChange={(e) => handle(e)} placeholder="Şifre" id="password" type="password" className="form-control" name="password" value={data.password} required data-eye />
+                                                <div className="invalid-feedback">
+                                                    Şifre Zorunludur!!
+                                                </div>
+                                            </div>
+                                            <div className="form-group">
+                                                {/* <label htmlFor="password">Şifre */}
+                                                
+                                                {/* </label> */}
+                                                <input onChange={(e) => handle(e)} placeholder="Tekrar Şifre" id="newpassword" type="newpassword" className="form-control" name="newpassword" value={data.newpassword} required data-eye />
+                                                <div className="invalid-feedback">
+                                                   Tekrar Şifre Zorunludur!!
+                                                </div>
+                                            </div>
+                                            <div className="form-group m-0">
+                                                <button type="submit" className="btn btn-primary btn-block">
+                                                    Kayıt Ol
+                                                </button>
+                                            </div>
+                                           
+                                        </form>
+                                    </div>
+                                </div>
+                                <div className="footer">
+                                    Copyright &copy; 2022 &mdash; Workup
                                 </div>
                             </div>
-
-                            <div className="mb-3">
-                                <label className="mb-1 text-body" htmlFor="name">LastName</label>
-                                <br></br>
-                                <input onChange={(e)=> handle(e)} id="lastname" value={data.lastname} placeholder="Enter your lastname here..." type="text"></input>
-                                <div className="invalid-feedback">
-                                    Name is required	
-                                </div>
-                            </div>
-
-                            <div className="mb-3">
-                                <label className="mb-1 text-body " htmlFor="email">E-Mail Address</label>
-                                <br></br>
-                                <input onChange={(e)=> handle(e)} id="email" value={data.email} placeholder="Enter your email here..." type="text"></input>
-                                <div className="invalid-feedback">
-                                    Email is invalid
-                                </div>
-                            </div>
-
-                            <div className="mb-3">
-                                <label className="mb-1 text-body " htmlFor="password">Password</label>
-                                <br></br>
-                                <input onChange={(e)=> handle(e)} id="password" value={data.password} placeholder="Enter your password here..." type="text"></input>
-                                <div className="invalid-feedback">
-                                    Password is required
-                                </div>
-                            </div>
-
-                            <div className="mb-3">
-                                <label className="mb-1 text-body" htmlFor="password">NewPassword</label>
-                                <br></br>
-                                <input onChange={(e)=> handle(e)} id="newpassword" value={data.newpassword} placeholder="Enter your re-password here..." type="text"></input>
-                                <div className="invalid-feedback">
-                                    Password is required
-                                </div>
-                            </div>
-
-                            <p className="form-text text-muted mb-3">
-                                By registering you agree with our terms and condition.
-                            </p>
-
-                            <div className="align-items-center d-flex">
-                                <button type="submit" className="btn btn-primary ms-auto">
-                                Submit
-                                </button>
-                            </div>
-                            <br></br>
-                            <div className="card-footer py-3 border-0">
-                        <div className="text-center">
-                            Already have an account? <a href="index.html" className="text-dark">Login</a>
                         </div>
-                    </div> 
-                        </form>
                     </div>
-                    {/* <div class="card-footer py-3 border-0">
-                        <div class="text-center">
-                            Already have an account? <a href="index.html" class="text-dark">Login</a>
-                        </div>
-                    </div> */}
-                    
-                </div>
-                {/* <div class="text-center mt-5 text-muted">
-                    Copyright &copy; 2017-2021 &mdash; Your Company 
-                </div> */}
+                </section>
             </div>
-        </div>
-    </div>
-</section>
-  )
+        </Container>
+    )
 }
+const Container = styled.div`
+  padding: 0px;
+`;
+const Nav = styled.nav`
+max-width: 1128px;
+margin: auto;
+padding: 12px 0 16px;
+display: flex;
+align-items: center;
+position: relative;
+justify-content: space-between;
+flex-wrap: nowrap;
 
+& > a {
+  width: 135px;
+  height: 34px;
+  @media (max-width: 768px) {
+    padding: 0 5px;
+  }
+}
+`;
 
 
 export default UserRegister;
